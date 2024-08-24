d06575a3350fa3def533862ee70635ab696036d6
hexis <hexis@openBSD.lan>
2024-08-24 01:52:23 -0400

    fixed install

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ printf "would you like to install openpkg (y/n)\n"
 read i
 
 if [ "$i" == "y" ]; then
-	cd ~/ && git clone https://github.com/hexisXz/openpkg.git && cd ~/openpkg && make && mkdir ~/.config/openpkg/ && mv ~/openpkg/update ~/.config/openpkg/ && mv ~/openpkg/uninstall ~/.config/openpkg/ && sudo rm -r ~/openpkg
+	cd ~/ && git clone https://github.com/hexisXz/openpkg.git && cd ~/openpkg && make && mkdir ~/.config/openpkg/ && mv ~/openpkg/update ~/.config/openpkg/ && mv ~/openpkg/uninstall ~/.config/openpkg/ && doas rm -r ~/openpkg
 else 
 	exit 1
 fi
